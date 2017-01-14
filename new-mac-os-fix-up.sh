4d2d764709198ed73226a426f22d5c9fde7f89cd
Vladislav Khvostov <vladkhvo@gmail.com>
2017-01-14 17:36:09 +0300

    Update new-mac-os-fix-up.sh

--- new-mac-os-fix-up.sh
+++ new-mac-os-fix-up.sh
@@ -3,6 +3,9 @@
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# install fish shell
+brew install fish
+
 # install spf-13 for vim
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 
